932efe46eef236f4cb8e46bfc3dcd9e21b962220
Luis Najera <lutech1@mac.com>
2015-07-13 10:15:54 -0400

    Updated Several sections with content. Added several JS and CSS  libraries.

--- application/views/emergency/emergencyinformation.php
+++ application/views/emergency/emergencyinformation.php
@@ -1,25 +1,3 @@
-
-
-<!-- Main jumbotron for a primary marketing message or call to action -->
-<div class="jumbotron">
-    <h1><?php echo $pagename ?></h1>
-</div>
-
-<!-- Example row of columns -->
 <div class="row">
-    <div class="col-md-4">
-        <ul>
-            <li>Report an Event</li>
-            <li>Allergies</li>
-            <li>Medications</li>
-            <li>Emergency Contacts</li>
-            <li>Medical Contacts</li>
-            <li>Representatives</li>
-            <li>Emergency Backup Providers</li>
-        </ul>
-    </div>
-    <div class="col-md-4">
-    </div>
-    <div class="col-md-4">
-    </div>
+    <?php require "emergencyinformation/emergencyInformationDetails.php" ?>
 </div>
---
+++ application/views/emergency/emergencyinformation/emergencyInformationDetails.php
@@ -0,0 +1,8 @@
+<div class="col-md-12">
+    <div class="block app-block">
+        <div class="header">
+            <h1>Emergency Information</h1>
+        </div>
+    </div>
+    <?php require "_EmergencyInformationContent.php" ?>
+</div>
