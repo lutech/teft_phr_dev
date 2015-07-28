1863b7e82ac553b90f1c2dafbad2569675e51995
Luis Najera <lutech1@mac.com>
2015-07-28 15:05:24 -0400

    Updated Several sections with content. Added several JS and CSS  libraries.

--- application/views/billing/serviceclaims/_ServiceClaimsContent.php
+++ application/views/billing/serviceclaims/_ServiceClaimsContent.php
@@ -18,7 +18,7 @@
         <td>$25.00</td>
         <td>Original</td>
         <td>Paid</td>
-        <td><a href="#" onclick="callServiceAcitivityDialogAlt()">View</a></td>
+        <td><a href="#" onclick="callServiceAcitivityDialogAlt()">Review</a></td>
     </tr>
     <tr>
         <td>04/08/2015</td>
@@ -27,7 +27,7 @@
         <td>$70.00</td>
         <td>Adjustment</td>
         <td>Paid</td>
-        <td><a href="#">View</a></td>
+        <td><a href="#">Review</a></td>
     </tr>
     <tr>
         <td>04/21/2015</td>
@@ -36,7 +36,7 @@
         <td>$0.00</td>
         <td>Original</td>
         <td><span class="icon-warning-sign"></span>Rejected</td>
-        <td><a href="#">View</a></td>
+        <td><a href="#">Review</a></td>
     </tr>
     </tbody>
 </table>
